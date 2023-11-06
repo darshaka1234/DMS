bd33ea39cdeb6b2e414924e3e6806ef18e501f85
darshaka1234 <darshakasrimal1234@gmail.com>
2023-11-06 21:45:06 +0530

    data fetch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategories, getAllNews } from "./../app/Api";
+import NewsCard from "../components/NewsCard";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllNews());
+    dispatch(getAllCategories());
+  }, []);
+
+  const NewsData = useSelector(state.news.data);
+
+  return (
+    <div>
+      {NewsData?.map((item) => (
+        <NewsCard data={item} />
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
